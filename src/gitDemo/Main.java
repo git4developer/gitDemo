154df8582e42cca2e1896dcc987aa8800af7fef7
pc <pc@DESKTOP-3R1QA8S>
2023-08-22 00:23:30 +0530

    second commit

--- src/gitDemo/Main.java
+++ src/gitDemo/Main.java
@@ -1,5 +1,14 @@
 package gitDemo;
 
 public class Main {
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("yes i am hungry");
+			}else {
+				System.out.println("no i am not hungry");
+			}
+		}
 
 }
